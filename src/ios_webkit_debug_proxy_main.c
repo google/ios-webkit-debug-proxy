a1d4ff5aec4ddb5bcff6b4fa58d448910949289e
Paul Irish <paul.irish@gmail.com>
2015-10-04 17:03:45 -0700

    DevTools frontend: change default from v18 to v27 https://github.com/google/ios-webkit-debug-proxy/issues/86

--- src/ios_webkit_debug_proxy_main.c
+++ src/ios_webkit_debug_proxy_main.c
@@ -208,7 +208,7 @@ int iwdpm_configure(iwdpm_t self, int argc, char **argv) {
   };
   const char *DEFAULT_CONFIG = "null:9221,:9222-9322";
   const char *DEFAULT_FRONTEND =
-     "http://chrome-devtools-frontend.appspot.com/static/18.0.1025.74/devtools.html";
+     "http://chrome-devtools-frontend.appspot.com/static/27.0.1453.93/devtools.html";
 
   self->config = strdup(DEFAULT_CONFIG);
   self->frontend = strdup(DEFAULT_FRONTEND);
