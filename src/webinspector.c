a724e109a874422f822cbe43ff0effa19ffc29ab
artygus <hero@engineeriam.net>
2018-09-18 20:58:11 +0300

    increase websocket max body size fixes #274

--- src/webinspector.c
+++ src/webinspector.c
@@ -37,7 +37,7 @@
 #define MAX_RPC_LEN 8096 - 500
 
 // some arbitrarly limit, to catch bad packets
-#define MAX_BODY_LENGTH 1<<24
+#define MAX_BODY_LENGTH 1<<26
 
 struct wi_private {
   bool partials_supported;
